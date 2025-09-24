a67b17d74c1d924e40d35f7e329b4dd8d0ce6070
DefeatedPond <raidyn.bilotta@gmail.com>
2025-09-23 19:49:19 -0500

    Made everything work with the new producer clas

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 class Producer {
-    constructor(buyCost, multiplierCost, costScaling, multiplierScaling, baseProduction, amount, multiplier) {
+    constructor(buyCost, multiplierCost, costScaling, multiplierScaling, baseProduction, multiplier, amount) {
         this.buyCost = buyCost;
-        this.multiplier = multiplierCost;
+        this.multiplierCost = multiplierCost;
         this.costScaling = costScaling;
         this.multiplierScaling = multiplierScaling;
         this.baseProduction = baseProduction;
-        this.amount = amount;
         this.multiplier = multiplier;
+        this.amount = amount;
     }
 
     buy() {
         if (money >= this.buyCost) {
-            money -= this.buyCost;
+            changeMoney(-this.buyCost);
             this.buyCost *= this.costScaling;
             this.amount++;
             return true;
@@ -22,8 +22,8 @@ class Producer {
 
     upgrade() {
         if (money >= this.multiplierCost) {
-            money -= this.multiplierCost;
-            this.upgradeCost *= this.costScaling;
+            changeMoney(-this.multiplierCost);
+            this.multiplierCost *= this.costScaling;
             this.multiplier *= this.multiplierScaling;
             return true;
         } else {
@@ -34,48 +34,42 @@ class Producer {
     getAmount() {
         return this.amount;
     }
-    
+
+    getBuyCost() {
+        return this.buyCost;
+    }
+        
+    getMultiplierCost() {
+        return this.multiplierCost;
+    }
+
     harvest() {
-        flowers += this.amount * this.baseProduction * this.multiplier;
+        changeFlowers(this.amount * this.baseProduction * this.multiplier);
     }
 }
 
-
 let money = 0;
 let flowers = 0;
-<<<<<<< HEAD
-let gardener = new Producer(50, 50, 1.2, 1.2, 1, 1, 1);
-=======
-let gardenerCount = 0;
+let gardener = new Producer(50, 50, 1.2, 1.2, 1, 1, 0);
 let prestigeCount = 0;
-let gardenerCost = 100;
->>>>>>> origin/main
+
 
 function buyGardener() {
-    if (gardener.getAmount() === 0) {
-        window.setInterval(gardenersGardening, 1000);
-    }
 
-    //TODO: implement an actual cost once flowers are obtainable
-<<<<<<< HEAD
     if (gardener.buy()) {
         document.querySelector("#gardenerBuy > h2").innerHTML = `gardener \n (${gardener.getAmount()})`;
-=======
-    if (money >= gardenerCost) {
-        gardenerCount++;
-        document.querySelector("#gardenerBuy > #count").innerHTML = `(${gardenerCount})`;
-        money -= gardenerCost;
-        gardenerCost = Math.round(gardenerCost * 1.25);
-        document.querySelector("#gardenerBuy > #cost").innerHTML = '$'+`${gardenerCost}`;
-
->>>>>>> origin/main
+        document.querySelector("#gardenerBuy > #count").innerHTML = `(${gardener.getAmount()})`;
+        document.querySelector("#gardenerBuy > #cost").innerHTML = '$'+`${gardener.getBuyCost()}`;
     } else {
         alert("You can't afford a gardener right now!");
+    }
+
+    if(gardener.getAmount() > 0) {
+        window.setInterval(() => gardener.harvest(), 1000);
     }
 }
 
 function increaseMultiplier(multiplier) {
-    // if you want to add anothe
     switch (multiplier) {
         case 'gardener':
             if (gardener.upgrade()) {
@@ -87,21 +81,10 @@ function increaseMultiplier(multiplier) {
     }
 }
 
-
-function gardenersGardening(){
-<<<<<<< HEAD
-    gardener.harvest();
-=======
-    changeFlowers(gardenerCount);
->>>>>>> origin/main
-    console.log(flowers);
-}
-
 function test(text) {
-    window.setInterval(gardenersGardening, 1000);
-    document.querySelector("#gardenerBuy > h2").innerHTML = `gardener \n (${gardener.getAmount()})`;
-    console.log(flowers);
+    return;
 }
+
 
 // 1 to 1 conversion babyyyyy
 function flowerToMoney() {
@@ -109,8 +92,6 @@ function flowerToMoney() {
     changeFlowers(-flowers);
     console.log("$"+money);
 }
-<<<<<<< HEAD
-=======
 
 //changes flower count by a given value
 function changeFlowers(amount) {
@@ -121,7 +102,7 @@ function changeFlowers(amount) {
 //changes money count by a given value
 function changeMoney(amount) {
     money += amount;
-    document.getElementById("money_display").innerHTML = "Money: " + money;
+    document.getElementById("dollar_display").innerHTML = "Money: " + money;
 }
   
 function prestige() {
@@ -131,4 +112,3 @@ function prestige() {
     prestigeCount += 1
     console.log("prestige count: " + prestigeCount)
 }
->>>>>>> origin/main
